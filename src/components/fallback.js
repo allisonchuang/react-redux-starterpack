fa600aaf04fa3d905549a01f856f0f7c5f08b012
allisonchuang <allison.p.chuang.19@dartmouth.edu>
2017-04-29 14:11:22 -0400

    completed assignment

--- src/components/fallback.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import '../style.scss';
-
-const FallBack = (props) => {
-  return <div>URL Not Found</div>;
-};
-
-export default FallBack;
--- src/components/test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import '../style.scss';
-
-const Test = (props) => {
-  return <div> ID: {props.match.params.id} </div>;
-};
-
-export default Test;
